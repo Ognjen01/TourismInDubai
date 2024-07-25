486dcdbabed38fd62d26e9cd711e0f8ea19c5a55
Ognjen Lazić <lazicognjen2001@gmail.com>
2024-07-25 09:58:36 +0200

    T10 | Add animations (#8)

--- app/src/main/java/com/ognjenlazic/tourismindubai/ui/components/CategorySectionTIle.kt
+++ app/src/main/java/com/ognjenlazic/tourismindubai/ui/components/CategorySectionTIle.kt
@@ -1,6 +1,9 @@
 package com.ognjenlazic.tourismindubai.ui.components
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -8,6 +11,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ognjenlazic.tourismindubai.ui.theme.NotificationRed
@@ -19,44 +23,70 @@ fun CategorySectionTile(
     emoji: String,
     text: String,
     isNotificationAvailable: Boolean,
-    onClick: () -> Unit
 ) {
+    var isFlipped by remember { mutableStateOf(false) }
+    val rotation by animateFloatAsState(if (isFlipped) 180f else 0f, label = "")
+    val interactionSource = remember { MutableInteractionSource() }
+
     Card(
         shape = RoundedCornerShape(36.dp),
         colors = CardDefaults.cardColors(
             containerColor = TileGray,
         ),
         modifier = Modifier
-            .wrapContentSize(),
-        onClick = onClick
+            .wrapContentSize()
+            .graphicsLayer {
+                rotationY = rotation
+                cameraDistance = 12f * density
+            }
+            .clickable(
+                interactionSource = interactionSource,
+                indication = null
+            ) {
+                isFlipped = !isFlipped
+            }
     ) {
         Box(
             modifier = Modifier.padding(horizontal = 18.dp, vertical = 8.dp)
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center,
-                modifier = Modifier.padding(8.dp)
-            ) {
-                Box {
+            if (rotation < 90f || rotation > 270f) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Box {
+                        Text(
+                            text = emoji,
+                            style = Typography.labelSmall
+                        )
+                        if (isNotificationAvailable) {
+                            Box(
+                                modifier = Modifier
+                                    .size(8.dp)
+                                    .background(NotificationRed, shape = CircleShape)
+                                    .align(Alignment.TopEnd)
+                            )
+                        }
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
                     Text(
-                        text = emoji,
+                        text = text,
                         style = Typography.labelSmall
                     )
-                    if (isNotificationAvailable) {
-                        Box(
-                            modifier = Modifier
-                                .size(8.dp)
-                                .background(NotificationRed, shape = CircleShape)
-                                .align(Alignment.TopEnd)
-                        )
-                    }
                 }
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(
-                    text = text,
-                    style = Typography.labelSmall
-                )
+            } else {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "✅",
+                        style = Typography.labelSmall
+                    )
+                }
             }
         }
     }
@@ -69,6 +99,5 @@ fun CategorySectionTilePreview() {
         emoji = "🦜",
         text = "BIRDS",
         isNotificationAvailable = true,
-        onClick = {}
     )
 }
